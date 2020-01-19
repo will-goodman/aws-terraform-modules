f7275180443dc1d604991f7366fc6f704789d12e
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-01-19 22:05:09 +0000

    Added missing .id

--- vpc/main.tf
+++ vpc/main.tf
@@ -21,7 +21,7 @@ resource "aws_route_table_association" "public_association" {
 }
 
 resource "aws_route" "public_route" {
-  route_table_id = aws_route_table.public_route_table
+  route_table_id = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.internet_access.id
 }
