2af59d687291cc6c8dfd615c4d9115cac019b746
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-01-19 22:16:43 +0000

    Updated gateway_id to nat_gateway_id in private route

--- vpc/main.tf
+++ vpc/main.tf
@@ -53,7 +53,7 @@ resource "aws_route_table_association" "private_association" {
 resource "aws_route" "private_route" {
   route_table_id = aws_route_table.private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.nat_gateway.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway.id
 }
 
 resource "aws_eip" "nat_eip" {
