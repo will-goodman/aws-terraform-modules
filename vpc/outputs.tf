cb89eb1650a0236ba7396e808c0e4ac0b246db2f
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-02-02 17:26:26 +0000

    Updated VPC outputs.tf to return both public and private subnets.

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -3,10 +3,10 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "public_subnet" {
-  value = aws_subnet.public_subnet.id
+output "public_subnets" {
+  value = [aws_subnet.public_subnet.id, aws_subnet.second_public_subnet.id]
 }
 
-output "private_subnet" {
-  value = aws_subnet.private_subnet.id
+output "private_subnets" {
+  value = [aws_subnet.private_subnet.id, aws_subnet.second_private_subnet.id]
 }
