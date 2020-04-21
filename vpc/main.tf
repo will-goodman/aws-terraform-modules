2776d48321c0111d0c0cbac9e8b5476f0f08dd1a
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-04-21 20:05:11 +0100

    Removed private route table

--- vpc/main.tf
+++ vpc/main.tf
@@ -60,22 +60,3 @@ resource "aws_subnet" "second_private_subnet" {
 
   availability_zone = var.availability_zones[1]
 }
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_route_table_association" "private_association" {
-  route_table_id = aws_route_table.private_route_table.id
-  subnet_id = aws_subnet.private_subnet.id
-}
-
-resource "aws_route_table_association" "second_private_association" {
-  route_table_id = aws_route_table.private_route_table.id
-  subnet_id = aws_subnet.second_private_subnet.id
-}
-
-resource "aws_route" "private_route" {
-  route_table_id = aws_route_table.private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-}
